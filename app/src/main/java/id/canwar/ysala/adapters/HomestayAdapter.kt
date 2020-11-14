51f0f236e22078f7cb6fbb3d622a6c0840cf458f
gabrielgnr <56532692+gabrielgnr@users.noreply.github.com>
2020-11-14 13:56:07 +0700

    Adapter Homestay Change 2

--- app/src/main/java/id/canwar/ysala/adapters/HomestayAdapter.kt
+++ app/src/main/java/id/canwar/ysala/adapters/HomestayAdapter.kt
@@ -42,7 +42,7 @@ class HomestayAdapter(val context: Context, val homestays: ArrayList<Homestay>) 
 
             }
 
-            // bug view tidak bisa click
+
             view.homestay_placeholder.setOnClickListener {
                 Log.e("click view", "clicked")
 
