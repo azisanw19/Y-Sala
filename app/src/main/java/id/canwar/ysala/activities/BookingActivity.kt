8b426b6b0051d4931fb2e517063081525291db10
Aziz Anwar <azisanw19@gmail.com>
2020-11-14 12:59:56 +0700

    activity booking ^^belum selesai

--- app/src/main/java/id/canwar/ysala/activities/BookingActivity.kt
+++ app/src/main/java/id/canwar/ysala/activities/BookingActivity.kt
@@ -1,9 +1,13 @@
 package id.canwar.ysala.activities
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatTextView
 import com.squareup.picasso.Picasso
 import id.canwar.ysala.R
 import id.canwar.ysala.helpers.*
@@ -38,6 +42,56 @@ class BookingActivity : AppCompatActivity() {
             setupDatePicker(tv_checkout)
         }
 
+        if (et_pick_up_location.text.toString() != "") {
+            tv_time_text.visibility = View.GONE
+            tv_time.visibility = View.GONE
+        } else {
+            tv_time_text.visibility = View.VISIBLE
+            tv_time.visibility = View.VISIBLE
+        }
+
+        val time = SimpleDateFormat("HH:mm", Locale.US).format(calendar.time)
+        tv_time.text = time
+
+        tv_time.setOnClickListener {
+            setupTimePicker(tv_time)
+        }
+
+        tv_payment.setOnClickListener {
+            val payments = resources.getStringArray(R.array.payment_method)
+            setupDialog(payments, tv_payment)
+        }
+
+        // Add logika hitung harga
+    }
+
+    private fun setupDialog(array: Array<String>, textView: TextView) {
+
+        val choice = array.indexOf(textView.text)
+
+        AlertDialog.Builder(this, R.style.DialogTheme)
+                .setSingleChoiceItems(array, choice) { dialog, which ->
+                    textView.text = array[which]
+                    dialog.dismiss()
+                }
+                .create()
+                .show()
+
+    }
+
+    private fun setupTimePicker(textView: TextView) {
+
+        val calendar = Calendar.getInstance()
+        calendar.time = SimpleDateFormat("HH:mm", Locale.US).parse(textView.text.toString())
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+            calendar.apply {
+                set(Calendar.HOUR_OF_DAY, hourOfDay)
+                set(Calendar.MINUTE, minute)
+            }
+            textView.text = SimpleDateFormat("HH:mm", Locale.US).format(calendar.time)
+        }
+
+        TimePickerDialog(this, R.style.DialogTheme, timeSetListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show()
     }
 
     private fun setupDatePicker(textView: TextView) {
